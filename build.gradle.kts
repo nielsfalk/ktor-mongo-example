3aafcef7ea6561c28cd8fa6f157ff9e3dd74eaca
Niels Falk <niels@nielsfalk.de>
2023-12-08 22:04:16 +0100

    add mongodb-driver-kotlin-coroutine dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val mongo_driver_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.21"
@@ -29,6 +30,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:$mongo_driver_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
