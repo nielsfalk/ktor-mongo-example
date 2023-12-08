e259af01bce49616333262210efb31bbabf28da2
Niels Falk <niels@nielsfalk.de>
2023-12-08 22:34:58 +0100

    add kotest dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val mongo_driver_version: String by project
+val kotest_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.21"
@@ -33,4 +34,10 @@ dependencies {
     implementation("org.mongodb:mongodb-driver-kotlin-coroutine:$mongo_driver_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation ("io.kotest:kotest-runner-junit5:$kotest_version")
+    testImplementation ("io.kotest:kotest-assertions-core:$kotest_version")
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
